2b6fd2ac2983255bb234892049642ac1a1751143
sagacious-shell-7968 <tommysytc.tc@gmail.com>
2020-12-29 06:20:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function reduce(arr, funct, start){
   let r = (!!start) ? start : arr[0]
   let i = (!!start) ? 0 : 1
 
-  for (; i < src.length; i++) {
+  for (; i < arr.length; i++) {
     r = funct(arr[i], r)
   }
 
